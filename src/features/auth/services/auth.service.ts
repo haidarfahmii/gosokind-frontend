d9d71f8ee1f52ba067aa09ff69eccb0702bc5c06
rafafadhila <rafafadhila03@gmail.com>
2026-02-06 14:07:07 +0700

    feat: add service for forgot and reset password

--- src/features/auth/services/auth.service.ts
+++ src/features/auth/services/auth.service.ts
@@ -1,6 +1,6 @@
 // Pastikan Anda sudah punya axiosInstance, jika belum buat di src/lib/axios.ts
 import axiosInstance from "@/utils/axiosInstance";
-import { RegisterFormValues, VerifyFormValues } from "@/@types";
+import { ForgotPasswordFormValues, RegisterFormValues, ResetPasswordFormValues, VerifyFormValues } from "@/@types";
 
 export const authService = {
     async register(data: RegisterFormValues) {
@@ -30,5 +30,23 @@ export const authService = {
             skipAuthRedirect: true,
         } as any);
         return response.data;
-    }
+    },
+
+    async forgotPassword(data: ForgotPasswordFormValues) {
+        const response = await axiosInstance.post("/auth/forgot-password", {
+            email: data.email,
+        });
+        return response.data;
+    },
+
+    async resetPassword(data: ResetPasswordFormValues) {
+         const { token, ...body } = data;
+        // Backend mengharapkan body: { password } Dan Header: Authorization: Bearer <token>
+        const response = await axiosInstance.post("/auth/reset-password", body, {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        });
+        return response.data;
+    },
 };
